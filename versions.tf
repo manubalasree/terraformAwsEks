1dddaf3b7b2ff9bf36f2c156334a1d3d3fbb952d
Manu Balasree <manubalasree@gmail.com>
2021-09-26 10:01:20 +0530

    added versions

---
+++ versions.tf
@@ -0,0 +1,27 @@
+/*
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 2.0.0"
+    }
+  }
+
+  required_version = ">= 0.14"
+}
+*/
+terraform {
+  required_version = ">= 0.14"
+}
+
+# version constraints
+required_providers {
+  local      = ">= 1.4"
+  random     = ">= 2.1"
+  kubernetes = ">= 1.18, <= 1.20"
+}
