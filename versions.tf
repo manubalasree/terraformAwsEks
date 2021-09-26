7f696065cb72aba8ac1ce194f78b1b97456bde18
Manu Balasree <manubalasree@gmail.com>
2021-09-26 18:27:08 +0530

    bugs

--- versions.tf
+++ versions.tf
@@ -16,12 +16,11 @@ terraform {
 }
 */
 terraform {
-
+  required_version = ">= 0.14"
 # version constraints
   required_providers {
     local      = ">= 1.4"
     random     = ">= 2.1"
-    kubernetes = ">= 1.18, <= 1.20"
+    kubernetes = "<= 1.20"
   }
-  required_version = ">= 0.14"
 }
