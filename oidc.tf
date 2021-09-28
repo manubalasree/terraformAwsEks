0c77bc1f628b39ce2d595139b25613a339aa47ba
Manu Balasree <manubalasree@gmail.com>
2021-09-28 14:50:28 +0530

    bugs

--- oidc.tf
+++ oidc.tf
@@ -13,7 +13,10 @@ module "iam_policy" {
     "Version": "2012-10-17",
     "Statement": [
       {
-        "Action": ["s3:ListBucket"],
+        "Action": [
+          "s3:ListBucket",
+          "GetObject",
+       ],
         "Effect": "Allow",
         "Resource": "*"
       },
